50b64693698252687247ea2b22d68fbc555ca981
imakhileshsahu <akhileshsahunpic@gmail.com>
2026-04-01 03:30:28 +0530

    fix cors allow authorization header properly

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	// }))
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3001, https://blog-website-akhi.netlify.app",
+		AllowOrigins:     "https://blog-website-akhi.netlify.app",
 		AllowMethods:     "GET,POST,PUT,DELETE,OPTIONS",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowCredentials: true,
